af6925facec7a6fcfc11c04c974fda22c0da758a
Elias Ortega <eliass_56@hotmail.com>
2021-03-05 15:32:25 -0500

    Updates

---
+++ precache-manifest.c77d2f03fa97a76104db2839104938af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68e3d189169f57c774013b51dfda682",
+    "url": "./index.html"
+  },
+  {
+    "revision": "31ac0647d1ee5ab2da6f",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "08d942835ef5c0f23c04",
+    "url": "./static/js/2.ab65a008.chunk.js"
+  },
+  {
+    "revision": "28d36cae8db7790f6fc02920470bed26",
+    "url": "./static/js/2.ab65a008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ac0647d1ee5ab2da6f",
+    "url": "./static/js/main.05f3340f.chunk.js"
+  },
+  {
+    "revision": "478be64af20f7a103d10",
+    "url": "./static/js/runtime-main.7fdacd25.js"
+  }
+]);
